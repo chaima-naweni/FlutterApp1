de3df6454f9e84ec6297599f8bfad96251cd4fcb
chaima-naweni <chaima.naoueni14@gmail.com>
2021-11-28 19:23:26 +0100

    ScreenArguments

---
+++ lib/ScreenArguments.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names
+
+class ScreenArguments {
+  final String title;
+  final String message;
+
+  // ignore: non_constant_identifier_names
+  ScreenArguments(this.title, this.message);
+}
